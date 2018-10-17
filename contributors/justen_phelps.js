c912c6e2a65f4caaebba6f1fbbda84fe12953cd8
Justen Phelps <phelps.justen@gmail.com>
2018-10-16 23:41:17 -0700

    Added my contributor file!
    Simple console.log js file with some of my info for this shoutout!

---
+++ contributors/justen_phelps.js
@@ -0,0 +1,11 @@
+console.log(
+"Hacktoberfest 2018 \n"+
+"\n"+
+"-Justen Phelps  \n"+
+"-Twitter: @justenphelps \n"+                                                                                                                           
+"-Github: https://github.com/justenbphelps \n"+
+"\n"+
+"I'm interested in learning more React and Javascript projects. I mostly work with front-end applications in the MERN stack and am always up to learn more! \n"+
+"\n"+
+"Cheers!"
+)
